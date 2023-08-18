cd75c2ced1b8ebdf3e0c0428b59d40d7f2626279
amrit-romana <connectwithromana@gmail.com>
2023-08-18 20:21:51 +0530

    Pushing the Final Result

---
+++ src/pages/SignUps.js
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react'
+import './SignUps.css'
+import { useNavigate } from 'react-router-dom';
+
+function SignUps() {
+    const navigate = useNavigate();
+    // const emailref = useRef(null);
+    // const passref = useRef(null);
+
+
+    const register=(i)=>{
+        navigate('/homescreen');
+        return false;
+       
+    };
+    const signIn=(e)=>{
+        navigate('/homescreen');
+        // auth.createUserWithEmailAndPassword(
+        //     emailref.current.value,
+        //     passref.current.value
+        // ).then((authUser)=>{
+        //     console.log(authUser)
+        // }).catch((error)=>{
+        //     alert(error.message)
+        // });
+        return false;
+    };
+  return (
+    <div className='signups'>
+        <form onSubmit={signIn}>
+            <h1>Sign In</h1>
+            <input  placeholder='Email' type='email'/>
+            <input  placeholder='Password' type='password'/>
+            <button type="submit" >Sign In</button>
+            <h4><span className='signups_grey'>New to Netflix?</span><span className='signups_sign' onClick={register}> Sign Up now.</span></h4>
+        </form>
+    </div>
+  )     
+}
+
+export default SignUps
---
+++ src/pages/logins.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './logins.css'
+import SignUps from './SignUps';
+function Logins() {
+  const [signIn, setsignIN]=useState(false);
+  return (
+    <div className='logins'>
+      <div className='logins_background'>
+     <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" className='logins_logo'/>
+      <button className='logins_button' onClick={()=> setsignIN(true)}>Sign In</button>
+      <div className='logins_gradient'/>
+      <div className='logins_body'>
+        {signIn?(
+          <SignUps/>
+        ):
+              
+    <>
+    <h1>Unlimited movies, TV shows and more</h1>
+    <h2>Watch anywhere. Cancel anytime.</h2>
+    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+   <div className='logins_input'>
+    <form>
+      <input type='email' placeholder='Email Address'/>
+      <button className='logins_getsb' onClick={()=>setsignIN(true)}>Get Started</button>
+    </form>
+   </div>
+    </>
+        }
+   
+
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Logins
